54223b7ad21f0e6ad9a40ac39a0737771b30aef0
bansal-gbu <54972682+bansal-gbu@users.noreply.github.com>
2019-09-05 21:56:57 -0700

    ok

---
+++ arearect.cpp
@@ -0,0 +1,23 @@
+// made by Kartavya Bansal Bsc(H) Physics
+// roll no- 19/BHP/013
+// to find the area of a rectangle
+#include <iostream>
+using namespace std;
+int main()
+{
+	float	length,breadth, areaofrectangle;
+
+	cout<< "enter the value of length in metre " ;
+	cin>> length ;
+	
+	cout<< "enter the value of breadth in metre " ;
+	cin>> breadth;
+	
+	areaofrectangle= (length*breadth);		
+	cout<<"area of rectangle in (metre)^2 is " <<areaofrectangle ;
+	return 0;
+	
+
+
+
+}
